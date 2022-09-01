245ae2a066359f34510e8e1b4de3c764ff30015c
dwelch-spike <dwelch@aerospike.com>
2022-08-31 17:15:30 -0700

    fix: destroy records in record_batch_tracker_destory

--- src/batch_uploader.c
+++ src/batch_uploader.c
@@ -322,6 +322,11 @@ _record_batch_tracker_alloc(batch_uploader_t* uploader, as_vector* records)
 static void
 _record_batch_tracker_destroy(record_batch_tracker_t* tracker)
 {
+	for (int i = 0; i < tracker->records.size; i++) {
+		as_rec* rec_to_free = as_vector_get(&tracker->records, i);
+		as_rec_destroy(rec_to_free);
+	}
+
 	as_vector_destroy(&tracker->records);
 	cf_free(tracker);
 }
