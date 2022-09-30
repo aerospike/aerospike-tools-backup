2804c4eb0594735a831c29db5008f56b87fdecd4
atefehmohseni <aejiyeh@aerospike.com>
2022-09-30 11:00:19 -0700

    WIP added a unit test for reading/parsing user's info into a backup file

--- test/unit/test_backup_file.c
+++ test/unit/test_backup_file.c
@@ -18,7 +18,7 @@
 		ck_assert_int_eq(io_read_proxy_init(&io, file), 0); \
 	} while(0)
     
-START_TEST(test_enc_dec_text)
+START_TEST(test_enc_dec_sindex)
 {
     io_write_proxy_t wio;
 	WRITE_INIT_MATRIX(wio, TMP_FILE_0, 1);
@@ -44,7 +44,7 @@ START_TEST(test_enc_dec_text)
     index_param sindex_params2;
     as_vector ns_vec; //empty ns vector
 
-    res = text_parse(&rio, false, &ns_vec, NULL, &line_no[0], NULL, 0, false, &sindex_params2, NULL);
+    res = text_parse(&rio, false, &ns_vec, NULL, &line_no[0], NULL, 0, false, &sindex_params2, NULL, NULL);
     ck_assert_int_eq(res, DECODER_INDEX);
     io_proxy_close(&rio);
 
@@ -67,16 +67,60 @@ START_TEST(test_enc_dec_text)
 }
 END_TEST
 
+START_TEST(test_enc_dec_users_info)
+{
+    io_write_proxy_t wio;
+	WRITE_INIT_MATRIX(wio, TMP_FILE_0, 1);
+
+    as_user* test_user = cf_malloc(sizeof(as_user) + (2 * AS_ROLE_SIZE));
+	test_user->roles_size = 2;
+    strcpy(test_user->name, "test_user_name");
+
+	as_vector_init(&test_user->roles, sizeof (as_role), 25);
+    as_vector_append(&test_user->roles, "test_role_1");
+    as_vector_append(&test_user->roles, "test_role_2");
+
+    bool res_put = text_put_user_info(&wio, &test_user);
+    io_proxy_flush(&wio);
+    ck_assert(res_put); 
+
+    io_proxy_close2(&wio, FILE_PROXY_EOF);
+
+	io_read_proxy_t rio;
+	READ_INIT_MATRIX(rio, TMP_FILE_0, 1);
+
+    uint32_t line_no[1] = {1}; //only 1 record on file
+    decoder_status res_parse = DECODER_ERROR;
+
+    as_user* test_user_2 = cf_malloc(sizeof(as_user) + (2 * AS_ROLE_SIZE));
+    as_vector ns_vec; //empty ns vector
+    res_parse = text_parse(&rio, false, &ns_vec, NULL, &line_no[0], NULL, 0, false, NULL, NULL, &test_user_2);
+    ck_assert_int_eq(res_parse, DECODER_USER);
+    io_proxy_close(&rio);
+
+    ck_assert_str_eq(test_user->name, test_user_2->name);
+    ck_assert_int_eq(test_user->roles_size, test_user_2->roles_size); 
+    ck_assert_str_eq(test_user->roles[0], test_user_2->roles[0]);
+    ck_assert_str_eq(test_user->roles[1], test_user_2->roles[1]);
+
+    remove(TMP_FILE_0);
+    as_user_destroy(&test_user);
+    as_user_destroy(&test_user_2);
+}
+END_TEST
+
 Suite* backup_file_suite()
 {
     Suite* s;
 	TCase* tc_enc_dec_text;
 
-    s = suite_create("Backup file: encode/decode a secondary index with ctx");
+    s = suite_create("Backup file: encode/decode global records in backup file");
 
     tc_enc_dec_text = tcase_create("encode records in a backup file");
-	tcase_add_test(tc_enc_dec_text, test_enc_dec_text);
+	tcase_add_test(tc_enc_dec_text, test_enc_dec_sindex);
+    // test below needs to be completed with upcoming user info written on backup file
+    //tcase_add_test(tc_enc_dec_text, test_enc_dec_users_info);
     suite_add_tcase(s, tc_enc_dec_text);
-
+    
     return s;
 }
