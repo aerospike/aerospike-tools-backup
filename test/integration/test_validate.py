72861dd3602ef750469dd97dd07c06da55ac7fc6
dylan <dwelch@aerospike.com>
2023-06-30 13:43:52 -0700

    remove data written check

--- test/integration/test_validate.py
+++ test/integration/test_validate.py
@@ -7,42 +7,7 @@ Tests the representation of bin names in backup files.
 import lib
 import aerospike
 import subprocess
-from run_backup import backup_and_restore, restore_from_file
-
-SET_NAME = lib.SET
-BIN_NAME = "bin1"
-VAL = "val1"
-KEY = "key"
-INDEX_NAME = "idx1"
-UDF_PATH = ""
-
-def put_all():
-	lib.write_record(SET_NAME, KEY, BIN_NAME, VAL)
-	lib.create_integer_index(SET_NAME, BIN_NAME, INDEX_NAME)
-	content = "--[=======[\n" + "TEST" + "\n--]=======]\n"
-	UDF_PATH = lib.put_udf_file(content)
-	# time for the index and udf to be created
-
-def check_all():
-	assert lib.test_record(SET_NAME, KEY) is False
-	assert lib.check_index(SET_NAME, BIN_NAME, aerospike.INDEX_NUMERIC) is False
-
-	try:
-		lib.get_udf_file(UDF_PATH)
-	except aerospike.exception.UDFNotFound:
-		pass
-
-def test_validate():
-	"""
-	Test that --validate does not restore anything.
-	"""
-	backup_and_restore(
-		lambda context: put_all(),
-		None,
-		lambda context: check_all(),
-		restore_opts=["--validate"],
-		restore_delay=25
-	)
+from run_backup import restore_from_file
 
 def test_validate_bad_file():
 	"""
