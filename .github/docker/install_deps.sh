6d9270ccb2194b8933e02565c9879de9cf590824
grant <grant@iowntheinter.net>
2025-09-26 16:53:06 +0000

    fix aws el8 el9 builds

--- .github/docker/install_deps.sh
+++ .github/docker/install_deps.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 alias make='make -j8'
 
-BUILD_DEPS_AMAZON="cmake openssl-devel libcurl-devel libzstd-devel jansson-devel"
-BUILD_DEPS_REDHAT_8="gcc-c++ libtool wget cmake openssl-devel libcurl-devel libzstd-devel which autoconf libtool git" #readline-devel flex
-BUILD_DEPS_REDHAT_9="gcc-c++ libtool wget cmake openssl-devel libcurl-devel libzstd-devel which autoconf libtool git" #readline-devel flex
+BUILD_DEPS_AMAZON="gcc-c++ libtool wget cmake openssl-devel libcurl-devel libzstd-devel which autoconf git libidn2 libunistring libunistring-devel"
+BUILD_DEPS_REDHAT_8="gcc-c++ libtool wget cmake openssl-devel libcurl-devel libzstd-devel which autoconf git" #readline-devel flex
+BUILD_DEPS_REDHAT_9="gcc-c++ libtool wget cmake openssl-devel libcurl-devel libzstd-devel which autoconf git" #readline-devel flex
 BUILD_DEPS_UBUNTU="libpsl-dev autotools-dev automake libtool cmake pkg-config zlib1g-dev build-essential libssl-dev libzstd-dev libjansson-dev"
 BUILD_DEPS_DEBIAN="libpsl-dev autotools-dev automake libtool cmake pkg-config zlib1g-dev build-essential libssl-dev libzstd-dev libjansson-dev"
 FPM_DEPS_DEBIAN="ruby-rubygems make rpm git rsync binutils"
 FPM_DEPS_UBUNTU_2004="ruby make rpm git rsync binutils"
 FPM_DEPS_UBUNTU="ruby-rubygems make rpm git rsync binutils"
 FPM_DEPS_AMAZON="ruby rpmdevtools make git python3 python3-pip rsync"
-FPM_DEPS_REDHAT_8="ruby rpmdevtools make git python3 python3-pip rsync"
+FPM_DEPS_REDHAT_8="ruby rpm-build make git python3 python3-pip rsync"
 FPM_DEPS_REDHAT_9="ruby rpmdevtools make git python3 python3-pip rsync zlib zlib-devel"
 
 AWS_SDK_VERSION="1.10.55"
@@ -242,21 +242,82 @@ function install_deps_ubuntu24.04() {
 }
 
 function install_deps_redhat-el8() {
-  #todo redhat el9 does not have flex or readline-devel available in the yum repos
-  yum install -y https://rpmfind.net/linux/centos-stream/9-stream/AppStream/x86_64/os/Packages/readline-devel-8.1-4.el9."$(uname -m)".rpm
-  yum install -y https://rpmfind.net/linux/centos-stream/9-stream/AppStream/x86_64/os/Packages/flex-2.6.4-9.el9."$(uname -m)".rpm
+  dnf -y install $BUILD_DEPS_REDHAT_8 $FPM_DEPS_REDHAT_8
 
-  dnf -y install $BUILD_DEPS_REDHAT $FPM_DEPS_AMAZON
-  curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
-  mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
-  /opt/golang/go/bin/go install github.com/asdf-vm/asdf/cmd/asdf@v0.18.0
-  install /root/go/bin/asdf /usr/local/bin/asdf
-  asdf plugin add python https://github.com/asdf-community/asdf-python.git
-  dnf install -y gcc g++ make automake zlib zlib-devel libffi-devel openssl-devel bzip2-devel xz-devel xz xz-libs \
-                      sqlite sqlite-devel sqlite-libs
-  asdf install python 3.10.18
-  asdf set python 3.10.18
-  asdf exec pip install pipenv
+  cd /opt
+  wget https://ftp.gnu.org/pub/gnu/gettext/gettext-0.21.tar.gz
+  tar -zxvf gettext-0.21.tar.gz
+  cd gettext-0.21
+  autoconf
+  ./configure
+  make
+  make install
+
+  cd /opt
+  git clone  https://github.com/rockdaboot/libpsl.git
+  cd libpsl
+  git checkout 0.21.5
+  ./autogen.sh
+  ./configure
+  make
+  make install
+
+  cd /opt
+  wget  http://git.savannah.gnu.org/cgit/readline.git/snapshot/readline-master.tar.gz
+  tar -zxvf readline-master.tar.gz
+  cd readline-master
+  ./configure
+  make
+  make install
+
+  cd /opt
+  git clone https://github.com/akheron/jansson.git
+  cd jansson
+  autoreconf -i
+  ./configure
+  make
+  make install
+
+
+  cd /opt
+  git clone https://github.com/libuv/libuv
+  cd libuv
+  git checkout v1.42.0
+  sh autogen.sh
+  ./configure
+  make
+  make install
+  cd ..
+
+  cd /opt
+  wget https://github.com/westes/flex/releases/download/v2.6.4/flex-2.6.4.tar.gz
+  tar -zxvf flex-2.6.4.tar.gz
+  cd flex-2.6.4
+  ./configure
+  make
+  make install
+
+  cd /opt
+  git clone https://github.com/aws/s2n-tls.git
+  cd s2n-tls
+
+  cmake . -Bbuild \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=./s2n-tls-install
+  cmake --build build -j8
+  make install
+
+  cd /opt
+  git clone https://github.com/aws/aws-sdk-cpp.git
+  cd aws-sdk-cpp
+  git checkout $AWS_SDK_VERSION
+  git submodule update --init --recursive
+  mkdir build
+  cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_ONLY="s3" -DBUILD_SHARED_LIBS=OFF -DENABLE_TESTING=OFF -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_INSTALL_LIBDIR=lib -DENABLE_UNITY_BUILD=ON
+  make -C build -j8
+  cd build
+  make install
+
   gem install fpm
 }
 function install_deps_redhat-el9() {
@@ -349,37 +410,79 @@ function install_deps_amazon-2023() {
 
 
   cd /opt
+  wget https://ftp.gnu.org/pub/gnu/gettext/gettext-0.21.tar.gz
+  tar -zxvf gettext-0.21.tar.gz
+  cd gettext-0.21
+  autoconf
+  ./configure
+  make
+  make install
+
+  cd /opt
+  git clone  https://github.com/rockdaboot/libpsl.git
+  cd libpsl
+  git checkout 0.21.5
+  ./autogen.sh
+  ./configure
+  make
+  make install
+
+  cd /opt
+  wget  http://git.savannah.gnu.org/cgit/readline.git/snapshot/readline-master.tar.gz
+  tar -zxvf readline-master.tar.gz
+  cd readline-master
+  ./configure
+  make
+  make install
+
+  cd /opt
+  git clone https://github.com/akheron/jansson.git
+  cd jansson
+  autoreconf -i
+  ./configure
+  make
+  make install
+
+
+  cd /opt
   git clone https://github.com/libuv/libuv
   cd libuv
-  git checkout v1.43.0
+  git checkout v1.42.0
   sh autogen.sh
   ./configure
   make
   make install
   cd ..
 
-#  git clone https://github.com/curl/curl.git
-#  cd curl
-#  git checkout curl-7_81_0
-#  git submodule update --init --recursive
-#  mkdir build
-#  cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_SHARED_LIBS=OFF -DBUILD_CURL_EXE=OFF
-#  make -C build
-#  cd build
-#  make install
-#  cd ../..
+  cd /opt
+  wget https://github.com/westes/flex/releases/download/v2.6.4/flex-2.6.4.tar.gz
+  tar -zxvf flex-2.6.4.tar.gz
+  cd flex-2.6.4
+  ./configure
+  make
+  make install
 
+  cd /opt
+  git clone https://github.com/aws/s2n-tls.git
+  cd s2n-tls
+
+  cmake . -Bbuild \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=./s2n-tls-install
+  cmake --build build -j8
+  make install
+
+  cd /opt
   git clone https://github.com/aws/aws-sdk-cpp.git
   cd aws-sdk-cpp
   git checkout $AWS_SDK_VERSION
   git submodule update --init --recursive
   mkdir build
-  cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_ONLY="s3" -DBUILD_SHARED_LIBS=ON -DENABLE_TESTING=OFF -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_INSTALL_LIBDIR=lib
+  cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_ONLY="s3" -DBUILD_SHARED_LIBS=OFF -DENABLE_TESTING=OFF -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_INSTALL_LIBDIR=lib -DENABLE_UNITY_BUILD=ON
   make -C build
   cd build
   make install
-  cd ../..
-
+  cd
 
   gem install fpm
 }
