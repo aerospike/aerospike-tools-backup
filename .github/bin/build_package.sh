8b3beb82dbcbce8a5ee65bfa56932c9540ebc640
Akash Chandra <achandra@aerospike.com>
2026-07-29 10:03:06 -0700

    fix(pkg): self-contained airgap-safe packages; dynamic-dep allowlist guard (#164)

--- .github/bin/build_package.sh
+++ .github/bin/build_package.sh
@@ -2,6 +2,36 @@
 set -xeuo pipefail
 
 alias make='make -j8'
+
+function assert_dynamic_deps() {
+  local allowed="libc.so.6 libm.so.6 libpthread.so.0 libdl.so.2 librt.so.1
+    libgcc_s.so.1 libstdc++.so.6 libz.so.1 ld-linux-x86-64.so.2 ld-linux-aarch64.so.1"
+  case "$ENV_DISTRO" in
+    el8)
+      allowed+=" libcurl.so.4 libssl.so.1.1 libcrypto.so.1.1 libzstd.so.1"
+      ;;
+    el9|el10|amzn2023)
+      allowed+=" libcurl.so.4 libssl.so.3 libcrypto.so.3 libzstd.so.1"
+      ;;
+    ubuntu26.04)
+      allowed+=" libjitterentropy.so.3"
+      ;;
+  esac
+
+  local bin lib needed fail=0
+  for bin in asbackup asrestore; do
+    needed=$(readelf -d "bin/$bin" | awk '/\(NEEDED\)/ { gsub(/[][]/, "", $NF); print $NF }')
+    echo "$bin DT_NEEDED:" $needed
+    for lib in $needed; do
+      if ! printf '%s\n' $allowed | grep -qxF "$lib"; then
+        echo "$bin has unexpected dynamic dependency $lib; link it statically or add it to the allowlist and the package depends" >&2
+        fail=1
+      fi
+    done
+  done
+  return $fail
+}
+
 function build_packages(){
 
   if [ "${ENV_DISTRO:-}" = "" ]; then
@@ -32,6 +62,8 @@ function build_packages(){
     fi
     make EVENT_LIB=libuv ZSTD_STATIC_PATH=/usr/lib/$ARCH-linux-gnu AWS_SDK_STATIC_PATH=/usr/local/lib CURL_STATIC_PATH=/usr/local/lib OPENSSL_STATIC_PATH=/usr/lib/$ARCH-linux-gnu AWS_SDK_STATIC_PATH=/usr/local/lib JANSSON_STATIC_PATH=/usr/lib/$ARCH-linux-gnu LIBUV_STATIC_PATH=/usr/local/lib
   fi
+
+  assert_dynamic_deps
 
   cd $PKG_DIR
   echo "building package for $BUILD_DISTRO"
